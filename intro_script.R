c8a24bb42d19cba9a01456c51912fecf1a1480c8
JYong-23-BOS <jyong@statisticsguyana.gov.gy>
2025-01-29 13:40:33 -0400

    The Data Scientist's Toolbox - Added intro_script.R
    Added intro_script.R to the drills folder in The Data Scientist's Toolbox course. This script includes a welcoming message, a simple data frame creation, and the data frame is displayed in the console as part of Drill 2

---
+++ intro_script.R
@@ -0,0 +1,7 @@
+#Print a welcoming message
+print("Welcome to the Data Science Specialization!")
+#Create a simple data frame
+names<- ("Jonathan,Kimberly,Billy,Jackson")
+age <- c(16,20,25,30,)
+profession <- c("Doctor","teacher","Police")
+df <- data.frame(names, age, profession)
